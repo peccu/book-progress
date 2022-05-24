7419e8be437c1c4d6331217879b777c5a765fa4c
Glitch (book-progress) <none>
2022-05-24 14:19:57 +0000

    ⏯️🟠 Updated with Glitch

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,23 @@ import { fileURLToPath, URL } from "url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
+import { createHtmlPlugin } from 'vite-plugin-html'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    createHtmlPlugin({
+      /**
+       * Data that needs to be injected into the index.html ejs template
+       */
+      inject: {
+        data: {
+          gtagId: process.env.VITE_GTAG_ID
+        }
+      }
+    }),
+  ],
   // for glitch generated static site
   build: {
     outDir: "build"
