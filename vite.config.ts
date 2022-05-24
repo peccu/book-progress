07f2a365a6ed7d289caec875b077cbcb099eea17
Glitch (book-progress) <none>
2022-05-24 13:12:38 +0000

    🧫📝 Updated with Glitch

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,10 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  // for glitch generated static site
+  build: {
+    outDir: "build"
+  },
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
