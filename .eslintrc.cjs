d0987225a2ebe7cec37f7d73de404126c00a513c
Glitch (book-progress) <none>
2022-05-25 23:05:14 +0000

    🎨🥒 Updated with Glitch

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,7 @@ require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
   root: true,
-  ignorePatterns: ["/build/*"],
+  /*ignorePatterns: ["/build/*"],*/
   extends: [
     "plugin:vue/vue3-essential",
     "eslint:recommended",
