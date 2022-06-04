0cbbfc3437f2fd8b533e8b1d8c2c9cb66c748af8
Glitch (book-progress) <none>
2022-06-04 11:04:24 +0000

    🚜😃 Updated with Glitch

--- netlify/functions/name.ts
+++ netlify/functions/name.ts
@@ -1,7 +1,7 @@
 import { Handler } from "@netlify/functions";
-/*
-const fetch = require("node-fetch");
 
+const fetch = require("node-fetch");
+/*
 const API_ENDPOINT = "https://icanhazdadjoke.com/";
 
 exports.handler = async (event, context) => {
