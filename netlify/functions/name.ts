cee1e6810695d0d53e7b5e05d4a29bcae5c3e0cd
Glitch (book-progress) <none>
2022-06-04 03:19:25 +0000

    🪞🛳️ Updated with Glitch

--- netlify/functions/name.ts
+++ netlify/functions/name.ts
@@ -1,11 +1,30 @@
 import { Handler } from "@netlify/functions";
 
+const html = (data) => `
+<html>
+<head>
+<title>book title - Book Progress</title>
+<meta property="og:title" content=たいとるやで>
+<meta property="og:site_name" content=さいとめいやけ>
+<meta property="og:url" content=https://google.com/?q=og>
+<meta property="og:description" content=descriptiondssssddd>
+<meta property="og:type" content=object>
+<meta property="og:image" content=imageurl>
+<script>// jump to original app page or, decode and display content
+</script>
+<body>
+Shared content ${data.name}
+</body>
+</html>
+`
+
+
 const handler: Handler = async (event, context) => {
   const name = event.queryStringParameters.name || "World";
-
+  const data = {name};
   return {
     statusCode: 200,
-    body: `<h1>Hello ${name}</h1>`,
+    body: html(data),
     body2: JSON.stringify({ message: `Hello, ${name}!` }),
   };
 };
