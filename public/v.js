69de7e0b1f0ff2519dfe92241eaf373ad16049ba
Glitch (book-progress) <none>
2022-06-16 16:49:22 +0000

    📑0️⃣ Updated with Glitch

--- public/v.js
+++ public/v.js
@@ -1,10 +1,10 @@
 (()=>{
   if(!localStorage.getItem('v')){
-    localStorage.setItem('v', 1655396913);
+    localStorage.setItem('v', 1655397626);
     return;
   }
-  if(localStorage.getItem('v') < 1655396913){
-    localStorage.setItem('v', 1655396913);
+  if(localStorage.getItem('v') < 1655397626){
+    localStorage.setItem('v', 1655397626);
     location.reload();
   }
 })()
