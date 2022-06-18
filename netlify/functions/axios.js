e13c9ab352456de45b24f36a94602167fd4be83d
Glitch (book-progress) <none>
2022-06-18 05:33:51 +0000

    🛎️🍩 Updated with Glitch

--- netlify/functions/axios.js
+++ netlify/functions/axios.js
@@ -36,7 +36,7 @@ exports.handler = function(event, context, callback) {
     callback(null, {
       statusCode: 200,
       body: JSON.stringify({
-        images: res,
+        images: res.data,
       }),
     })
   })
