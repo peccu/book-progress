89edaa7921e23bcd9301ac8100fa6d90037a10f9
Glitch (book-progress) <none>
2022-06-18 10:54:16 +0000

    🙏🩹 Updated with Glitch

---
+++ netlify/functions/axios.mjs
@@ -0,0 +1,35 @@
+// https://github.com/Chuloo/gatsby-netlify-functions/blob/master/src/lambda/fetch.js
+import axios from "axios";
+
+const search = async (isbn) => {
+  const url = "https://api.openbd.jp/v1/get?isbn=" + isbn.toString();
+  const res = await axios.get(url);
+  const book = genBook(res.data);
+  return book;
+};
+
+const genBook = (json) => {
+  const book = {};
+  const onix = json[0] && json[0];
+  const summary = json[0] && json[0].summary;
+  book.title = summary.title;
+  book.publisher = summary.publisher;
+  book.authors = summary.author.split(" ");
+  book.pages = json[0].onix?.DescriptiveDetail?.Extent[0].ExtentValue;
+  book.cover = summary.cover;
+  book.notes = json[0].onix?.CollateralDetail?.TextContent.map(
+    (e) => e.Text
+  ).join("\n\n");
+  return book;
+};
+
+export const handler = async function(event, context, callback) {
+  const isbn = event.queryStringParameters.isbn || "9784478109373";
+  console.log('Searching isbn: ' + isbn);
+  const book = await search(isbn);
+  console.log('found title: ' + book.title);
+  callback(null, {
+    statusCode: 200,
+    body: JSON.stringify(book),
+  });
+};
