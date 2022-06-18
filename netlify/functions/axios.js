9c5bf45bcf8f8191b57290af67bd362552f6796d
Glitch (book-progress) <none>
2022-06-18 10:19:00 +0000

    🦟💽 Updated with Glitch

--- netlify/functions/axios.js
+++ netlify/functions/axios.js
@@ -8,19 +8,20 @@ const search = async (isbn) => {
     "https://api.openbd.jp/v1/get?isbn=" + isbn.toString()
   )
   const json = await response.json();
-  const onix = json[0] && json[0];
-  // result.value = JSON.stringify(onix, null, 2);
-  const summary = json[0] && json[0].summary;
-  //  picked.value = summary;
-  book.title = summary.title;
-  book.publisher = summary.publisher;
-  book.authors = summary.author.split(" ");
-  book.pages = json[0].onix?.DescriptiveDetail?.Extent[0].ExtentValue;
-  book.cover = summary.cover;
-  book.notes = json[0].onix?.CollateralDetail?.TextContent.map(
-    (e) => e.Text
-  ).join("\n\n");
-  return book;
+  return json;
+  // const onix = json[0] && json[0];
+  // // result.value = JSON.stringify(onix, null, 2);
+  // const summary = json[0] && json[0].summary;
+  // //  picked.value = summary;
+  // book.title = summary.title;
+  // book.publisher = summary.publisher;
+  // book.authors = summary.author.split(" ");
+  // book.pages = json[0].onix?.DescriptiveDetail?.Extent[0].ExtentValue;
+  // book.cover = summary.cover;
+  // book.notes = json[0].onix?.CollateralDetail?.TextContent.map(
+  //   (e) => e.Text
+  // ).join("\n\n");
+  // return book;
 };
 
 exports.handler = function(event, context, callback) {
