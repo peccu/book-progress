b4111edfc992bc19c8cd30302c0e2cd33adaf65e
Glitch (book-progress) <none>
2022-06-18 13:43:43 +0000

    📢🐐 Updated with Glitch

--- v.sh
+++ v.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 cat v.js | sed 's/"VERSION_STR"/'$(date "+%s")'/g' > public/v.js
-curl -Lo netlify/functions/bookimage/NotoSerifCJKjp-VF.ttf https://github.com/googlefonts/noto-cjk/raw/main/Serif/Variable/TTF/NotoSerifCJKjp-VF.ttf
-
+curl -Lo \
+     netlify/functions/bookimage/NotoSerifJP-VF.ttf \
+     https://github.com/googlefonts/noto-cjk/raw/main/Serif/Variable/TTF/Subset/NotoSerifJP-VF.ttf
+# V over 50MB ^ 13MB
+# https://github.com/googlefonts/noto-cjk/raw/main/Serif/Variable/TTF/NotoSerifCJKjp-VF.ttf
