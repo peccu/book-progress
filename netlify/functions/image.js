f075be12d320def0e175692f53df9da9a6b62fea
Glitch (book-progress) <none>
2022-06-18 05:05:06 +0000

    🚇👋 Updated with Glitch

--- netlify/functions/image.js
+++ netlify/functions/image.js
@@ -9,7 +9,11 @@ exports.handler = async function (event, context) {
     headless: chromium.headless,
   });
   const page = await browser.newPage();
-  await page.setContent("<body>This is a <strong>Demo</strong></body>");
+  await page.setContent(`<body>This is a <strong>Demo</strong>
+  <svg height="210" width="500">
+  <polygon points="200,10 250,190 160,210" style="fill:lime;stroke:purple;stroke-width:1" />
+  Sorry, your browser does not support inline SVG.
+</svg></body>`);
   await page.waitForTimeout(1000);
   const buffer = await page.screenshot();
   return {
