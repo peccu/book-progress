62ed9adc7e8eb94ad0b8c3a7c448ef5f18b0122e
Glitch (book-progress) <none>
2022-06-18 15:24:28 +0000

    🐧📼 Updated with Glitch

--- netlify/functions/image/image.js
+++ netlify/functions/image/image.js
@@ -1,10 +1,3 @@
-// https://github.com/netlify/zip-it-and-ship-it/issues/525#issuecomment-858580934
-// https://github.com/spencewood/svg-function/pull/2/files
-console.log('default fontconfig path: ', process.env.FONTCONFIG_PATH);
-process.env.FONTCONFIG_PATH = "/var/task/functions/bookimage";
-process.env.FONTCONFIG_PATH = "/var/task/netlify/functions/image";
-// process.env.FONTCONFIG_PATH = "/app/netlify/functions/image";
-
 console.log('loaded');
 
 // https://michaelheap.com/netlify-function-lambda-return-image/
@@ -36,6 +29,15 @@ const genBook = (json) => {
 };
 
 exports.handler = async function (event, context) {
+// https://github.com/netlify/zip-it-and-ship-it/issues/525#issuecomment-858580934
+// https://github.com/spencewood/svg-function/pull/2/files
+console.log('default fontconfig path: ', process.env.FONTCONFIG_PATH);
+process.env.FONTCONFIG_PATH = "/var/task/functions/bookimage";
+process.env.FONTCONFIG_PATH = "/var/task/netlify/functions/image";
+// process.env.FONTCONFIG_PATH = "/app/netlify/functions/image";
+console.log('updated fontconfig path: ', process.env.FONTCONFIG_PATH);
+
+
 console.log('invoked');
 
   const browser = await puppeteer.launch({
