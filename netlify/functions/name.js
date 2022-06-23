55e8b5ebc6fe277294e160b16e24e5351810c44d
Glitch (book-progress) <none>
2022-06-23 05:32:40 +0000

    🐗🐏 Updated with Glitch

--- netlify/functions/name.js
+++ netlify/functions/name.js
@@ -36,13 +36,13 @@ const html = (data) => `
 <meta name="twitter:creator" content="@SarahMaslinNir">
 <meta name="twitter:title" content="Parade of Fans for Houston’s Funeral">
 <meta name="twitter:description" content="NEWARK - The guest list and parade of limousines with celebrities emerging from them seemed more suited to a red carpet event in Hollywood or New York than than a gritty stretch of Sussex Avenue near the former site of the James M. Baxter Terrace public housing project here.">
-<meta name="twitter:image" content="https://book-progress.netlify.app/.netlify/funvtions/image?debug&isbn=${data.isbn}">
+<meta name="twitter:image" content="https://book-progress.netlify.app/.netlify/functions/image?debug&isbn=${data.isbn}">
 <!--
 <meta name="twitter:card" content="summary_large_image">
 <meta name="twitter:creator" content="@peccul">
 <meta name="twitter:title" content="タイトルの進捗 - Book Progress">
 <meta name="twitter:description" content="タイトル 著書の進捗 : 現在23%">
-<meta name="twitter:image" content="https://book-progress.netlify.app/.netlify/funvtions/image?debug&isbn=${data.isbn}">
+<meta name="twitter:image" content="https://book-progress.netlify.app/.netlify/functions/image?debug&isbn=${data.isbn}">
 -->
 <script>// jump to original app page or, decode and display content
 </script>
