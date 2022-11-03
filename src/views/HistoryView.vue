67cd18fc8062091624d320413db73baf7da5771e
peccu <peccul+github@gmail.com>
2022-11-03 10:08:58 +0000

    add history page and button

---
+++ src/views/HistoryView.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import BookForm from "@/components/book/BookForm.vue";
+// import { useBooksState } from "@/stores/books";
+
+const props = defineProps({
+  id: String,
+});
+
+// const booksstore = useBooksState();
+console.log(`picked id: ${props.id}`);
+</script>
+
+<template>
+this is history view
+  <main>
+    <BookForm :id="id" />
+  </main>
+</template>
