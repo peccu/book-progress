e1c0282d7da533ae1a78ac57e8bc2a4b6c44dcc9
peccu <peccul+github@gmail.com>
2022-11-05 08:15:58 +0000

    move dateFloor function from history to date file

--- src/stores/date.ts
+++ src/stores/date.ts
@@ -1,5 +1,14 @@
 const zerofill = (s: number) => ("0" + s.toString()).slice(-2);
 
+// round by day
+const oneDayMs = 24 * 60 * 60 * 1000;
+export const dateFloor = (d: Date) => {
+  const offset = d.getTimezoneOffset();
+  return new Date(
+    Math.floor((d.getTime() + offset) / oneDayMs) * oneDayMs - offset
+  );
+};
+
 export const format = (date: number) => {
   const d = new Date(date);
   const y = d.getFullYear();
