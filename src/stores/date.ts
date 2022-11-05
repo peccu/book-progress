2dcb4ca6708314f0c628ec343fd067e414ce9f97
peccu <peccul+github@gmail.com>
2022-11-05 07:45:39 +0000

    change date format from UTC to Local

--- src/stores/date.ts
+++ src/stores/date.ts
@@ -2,9 +2,9 @@ const zerofill = (s: number) => ("0" + s.toString()).slice(-2);
 
 export default (date: number) => {
   const d = new Date(date);
-  const y = d.getUTCFullYear();
-  const m = zerofill(d.getUTCMonth() + 1);
-  const day = zerofill(d.getUTCDay());
+  const y = d.getFullYear();
+  const m = zerofill(d.getMonth() + 1);
+  const day = zerofill(d.getDate());
   const h = zerofill(d.getHours());
   const min = zerofill(d.getMinutes());
   const s = zerofill(d.getSeconds());
