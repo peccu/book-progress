804c7032802e0db47abb5fc5f82fb31bb7e68fdd
peccu <peccul+github@gmail.com>
2022-12-04 10:40:49 +0000

    extract date round by day function

--- src/stores/date.ts
+++ src/stores/date.ts
@@ -1,14 +1,5 @@
 const zerofill = (s: number) => `0${s.toString()}`.slice(-2);
 
-// round by day
-const oneDayMs = 24 * 60 * 60 * 1000;
-export const dateFloor = (d: Date) => {
-  const offset = d.getTimezoneOffset() * 60 * 1000;
-  return new Date(
-    Math.floor((d.getTime() - offset) / oneDayMs) * oneDayMs + offset,
-  );
-};
-
 export const format = (date: number) => {
   const d = new Date(date);
   const y = d.getFullYear();
